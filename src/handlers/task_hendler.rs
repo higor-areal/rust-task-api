ee1a1338ae2e128781d59006b862dfa65e934a47
Esau Higor <esauhigor@gmail.com>
2026-05-03 15:32:04 -0400

    Adicionando um terceiro path, new_task

--- src/handlers/task_hendler.rs
+++ src/handlers/task_hendler.rs
@@ -1,6 +1,16 @@
-use crate::models::task::Task;
+use crate::models::task::{Task, NewTask};
+use crate::responses::response::{ResponseNewTask, ResponseErro};
+use crate::state::app_state::AppState;
 
-use axum::Json;
+use std::sync::{
+    Arc,
+    Mutex
+};
+
+use axum::{
+    Json,
+    extract::State
+};
 
 pub async fn home() -> String{
 "Bem vindo ao servidor do higor".to_string()
@@ -8,4 +18,25 @@ pub async fn home() -> String{
 
 pub async fn echo(Json(item): Json<Task>) -> Json<Task>{
     Json(item)
+}
+
+pub async fn new_task(State(state): State<Arc<Mutex<AppState>>> , Json(item): Json<NewTask>) -> Result<Json<ResponseNewTask>, Json<ResponseErro>>{
+    if !item.valid(){
+        return Err(Json(ResponseErro{
+            status_code: 400,
+            message: "Task inválida".to_string()
+        }))
+    }
+    let mut data = state.lock().unwrap();
+
+    let temp = Task{
+        id: data.next_id, 
+        title: item.title.clone(), 
+        completed: item.completed
+    };
+
+    data.next_id +=1;
+    data.tasks.push(temp.clone());
+
+    Ok(Json(ResponseNewTask{status_code: 200, task: temp}))
 }
