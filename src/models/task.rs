4e403157f5b57fc68f8d38c9e02690e54c5ed9b1
Esau Higor <esauhigor@gmail.com>
2026-05-03 15:32:36 -0400

    criando struct NewTask, e impl  de Valid

--- src/models/task.rs
+++ src/models/task.rs
@@ -1,15 +1,21 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Clone)]
 pub struct Task{
     pub id: u32,
     pub title: String,
     pub completed: bool,
 }
 
-impl Task{
+#[derive(Deserialize, Serialize, Clone)]
+pub struct NewTask{
+    pub title: String,
+    pub completed: bool,
+}
+
+impl NewTask{
     pub fn valid(self: &Self) -> bool{
-        if self.completed || self.title.is_empty() { return false;}
+        if self.title.is_empty() { return false;}
         true
     }
 }
